660d1d90039512144104a2d2fa18eceb84253e57
degu0 <yatog2013@gmail.com>
2026-04-17 13:23:39 -0300

    style: update icons

--- frontend/src/app/_layout.tsx
+++ frontend/src/app/_layout.tsx
@@ -1,9 +1,21 @@
 import { AuthProvider } from '@/context/AuthContext';
 import { ThemeProvider } from '@/context/ThemeContext';
+import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
+void SplashScreen.preventAutoHideAsync();
+
 function LayoutWrapper() {
+  useEffect(() => {
+    const prepare = async () => {
+      await SplashScreen.hideAsync();
+    };
+
+    prepare();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Stack screenOptions={{ animation: 'none', headerShown: false }} />
