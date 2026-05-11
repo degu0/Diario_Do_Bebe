8c9b7ccbc6cff57bfec12157feb8104ec9c3c864
Davi Guilherme <Daviguilherme1904@gmail.com>
2026-05-11 14:38:30 -0300

    modified `schema.prisma` and `server.ts` to new controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import { bebeRoutes } from './routes/bebe.routes';
+import { diarioRoutes } from './routes/diario.routes';
+import { responsavelRoutes } from './routes/responsavel.routes';
+import { adiRoutes } from './routes/adi.routes';
+import { escolaRoutes } from './routes/escola.routes';
 
 const app = express();
 
@@ -7,6 +11,10 @@ app.use(express.json());
 
 // Registra as rotas de bebê com o prefixo /bebes
 app.use('/bebes', bebeRoutes);
+app.use('/diarios', diarioRoutes);
+app.use('/responsaveis', responsavelRoutes);
+app.use('/adis', adiRoutes);
+app.use('/escolas', escolaRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
